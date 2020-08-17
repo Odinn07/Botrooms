92c542f2299d29b2be8b88d0dae445ada4ff0efa
Deerzclub <41443929+Deerzclub@users.noreply.github.com>
2020-08-17 20:21:22 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = '!!';
+
+
+client.on('ready', () =>{
+    console.log('I have been resurrected');
+    client.user.setActivity('Minecraft Server - mc.Backrooms.me', { type:'PLAYING'}).catch(console.error);
+  
+})
+
+
+client.on('message', message=>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if(command === 'plug'){
+        message.channel.send('the plug from the backrooms image is a type C US plug');
+    } else if (command == 'info'){
+        message.channel.send("This is The Backrooms Investigation server where we try to solve the mystery of the location of the mysterious backrooms photo. While many of these liminal space photos like the backrooms could be real, there is some questioning of weather these photos are actually real. Some of them  turn out to be 3D renders. Could the backrooms be a 3D render? We do not know. If not, then there is still something to find out; where the location actually is.");
+    } 
+});
+
+
+client.login('NzQ0OTc1MDcxMDYzNDQxNTcw.XzrCGQ.kVH00SlyNyGjd97LAN_d4eUTFWY');
